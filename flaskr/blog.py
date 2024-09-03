7d83b3bcbf3eb132c680962df2aaa97e44f021e0
Kelechi Ebiri <ebirikele@gmail.com>
2024-09-03 12:07:34 +0100

    add files

---
+++ flaskr/blog.py
@@ -0,0 +1,66 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+
+from werkzeug.exceptions import abort
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        " SELECT p.id, title, body, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        "ORDER BY created DESC"
+        ).fetchall()
+    return render_template('blog/index.html', posts=posts)
+
+@bp.route('/create', methods=['GET', 'POST'])
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+
+    if not title:
+        error = 'Title is required'
+
+    if error is not None:
+        flash(error)
+    else:
+        db = get_db()
+        db.execute(
+            'INSERT INTO post (title, body, author_id)'
+            ' VALUES (?, ?, ?)',
+            (title, body, g.user['id'])
+        )
+
+        db.commit()
+
+        return render_template(url_for('blog.html'))
+    
+    return render_template('blog/create.html')
+
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELCECT p.id, title, body, created, author_id, username'
+        'FROM post p JOIN user n ON p.author_id = u.id'
+        'WHERE p.id = ?',
+        (id,)
+    ).fetchall()
+
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exists")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
